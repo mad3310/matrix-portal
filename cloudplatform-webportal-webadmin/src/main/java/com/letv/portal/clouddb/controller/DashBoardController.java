19886109b0f8d590a3018c5e6644a207f028a94c
liuhao1 <liuhao1@letv.com>
2014-12-17 17:37:58 +0800

    修改zabbix相关

--- cloudplatform-webportal-webadmin/src/main/java/com/letv/portal/clouddb/controller/DashBoardController.java
+++ cloudplatform-webportal-webadmin/src/main/java/com/letv/portal/clouddb/controller/DashBoardController.java
@@ -1,17 +1,21 @@
 package com.letv.portal.clouddb.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.annotation.Resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.letv.common.result.ResultObject;
 import com.letv.portal.proxy.IDashBoardProxy;
+import com.letv.portal.service.IContainerService;
+import com.letv.portal.zabbixPush.IZabbixPushService;
 
 @Controller
 @RequestMapping("/dashboard")
@@ -20,6 +24,11 @@ public class DashBoardController {
 	@Resource
 	private IDashBoardProxy dashBoardProxy;
 
+	@Resource
+	public IZabbixPushService zabbixPushService;
+	
+	@Resource
+	public IContainerService containerService;
 	
 	private final static Logger logger = LoggerFactory.getLogger(DashBoardController.class);
 	
@@ -38,5 +47,29 @@ public class DashBoardController {
 	public @ResponseBody ResultObject mclusterMonitor(ResultObject result) {
 		result.setData(this.dashBoardProxy.selectMclusterMonitor());
 		return result;
+	}
+	@RequestMapping(value="/zabbix/add",method=RequestMethod.GET)
+	public @ResponseBody ResultObject add(ResultObject result) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("mclusterId", 17);
+		zabbixPushService.createMultiContainerPushZabbixInfo(this.containerService.selectByMap(map));
+		return result; 
+	}
+	@RequestMapping(value="/zabbix/remove",method=RequestMethod.GET)
+	public @ResponseBody ResultObject remove(ResultObject result) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		   map.put("mclusterId", 17);
+			
+		   Boolean flag  =false;
+
+			try {
+				
+		     flag =	zabbixPushService.deleteMutilContainerPushZabbixInfo(this.containerService.selectByMap(map));
+		     System.out.println(flag);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		return result; 
 	}
 }
