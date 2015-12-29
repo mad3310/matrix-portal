0e784c540cae4131fad8e417107138aa8f6b92bb
liuhao1 <liuhao1@letv.com>
2015-12-29 12:02:39 +0800

    update httpClient

--- cloudplatform-webportal-data-model/src/main/java/com/letv/portal/python/service/impl/CbasePythonServiceImpl.java
+++ cloudplatform-webportal-data-model/src/main/java/com/letv/portal/python/service/impl/CbasePythonServiceImpl.java
@@ -1,8 +1,25 @@
 package com.letv.portal.python.service.impl;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.*;
 
+import org.apache.http.*;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.AuthCache;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.*;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -53,7 +70,7 @@ public class CbasePythonServiceImpl implements ICbasePythonService {
 		map.put("password", password);
 		map.put("port", port);
 
-		String result = HttpClient.postCbaseManager(url.toString(), map);
+		String result = HttpClient.post(url.toString(), map);
 		return new ApiResultObject(result,url.toString());
 	}
 
@@ -66,12 +83,98 @@ public class CbasePythonServiceImpl implements ICbasePythonService {
 
 		Map<String, String> map = new HashMap<String, String>();
 		map.put("memoryQuota", memoryQuota);
-
-		String result = HttpClient.postCbaseManager(url.toString(), nodeIp1,
-				port, map, adminUser, adminPassword);
+		String result = postCbaseManager(url.toString(), nodeIp1,
+                port, map, adminUser, adminPassword);
 		return new ApiResultObject(result,url.toString());
 	}
+	private static String postCbaseManager(String url, String ip, String port,
+										  Map<String, String> params, String username, String password) {
 
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+
+		String body = null;
+
+		HttpHost targetHost = new HttpHost(ip, Integer.valueOf(port), "http");
+
+		CredentialsProvider credsProvider = new BasicCredentialsProvider();
+		credsProvider.setCredentials(AuthScope.ANY,
+				new UsernamePasswordCredentials(username, password));
+
+		AuthCache authCache = new BasicAuthCache();
+		authCache.put(targetHost, new BasicScheme());
+
+		// Add AuthCache to the execution context
+		HttpClientContext context = HttpClientContext.create();
+		context.setCredentialsProvider(credsProvider);
+		context.setAuthCache(authCache);
+
+		logger.info("create httppost:" + url);
+		HttpPost post = postForm(url, params);
+
+		HttpResponse response = sendRequest(httpclient, targetHost, post,
+				context);
+		logger.info("get response from http server..");
+		if (response == null) {
+			logger.info("get response from http server.. faild");
+			return null;
+		}
+
+		HttpEntity entity = response.getEntity();
+
+		logger.info("response status: " + response.getStatusLine());
+
+		try {
+			logger.info(EntityUtils.toString(entity));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		body = Integer.toString(response.getStatusLine().getStatusCode());
+
+		httpclient.getConnectionManager().shutdown();
+
+		return body;
+	}
+    private static CloseableHttpResponse sendRequest(
+            CloseableHttpClient httpclient, HttpHost targetHost, HttpPost post,
+            HttpClientContext context) {
+        logger.info("execute post...");
+        CloseableHttpResponse response = null;
+        try {
+            response = httpclient.execute(targetHost, post, context);
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    private static HttpPost postForm(String url, Map<String, String> params) {
+        HttpPost httpost = new HttpPost(url);
+        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+
+        if (params != null && !params.isEmpty()) {
+            Set<String> keySet = params.keySet();
+            for (String key : keySet) {
+                nvps.add(new BasicNameValuePair(key, params.get(key)));
+                logger.info("param-->" + key + ":" + params.get(key));
+            }
+        }
+
+        try {
+            logger.info("set utf-8 form entity to httppost");
+            httpost.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return httpost;
+    }
 	@Override
 	public ApiResultObject addNodeToCluster(String srcNodeIp, String addNodeIp,
 			String port, String username, String password) {
@@ -84,8 +187,8 @@ public class CbasePythonServiceImpl implements ICbasePythonService {
 		map.put("user", username);
 		map.put("password", password);
 
-		String result = HttpClient.postCbaseManager(url.toString(), srcNodeIp,
-				port, map, username, password);
+		String result = postCbaseManager(url.toString(), srcNodeIp,
+                port, map, username, password);
 		return new ApiResultObject(result,url.toString());
 	}
 
@@ -100,8 +203,8 @@ public class CbasePythonServiceImpl implements ICbasePythonService {
 		map.put("ejectedNodes", "");
 		map.put("knownNodes", knownNodes);
 
-		String result = HttpClient.postCbaseManager(url.toString(), nodeIp,
-				port, map, username, password);
+		String result = postCbaseManager(url.toString(), nodeIp,
+                port, map, username, password);
 		return new ApiResultObject(result,url.toString());
 	}
 
@@ -112,7 +215,7 @@ public class CbasePythonServiceImpl implements ICbasePythonService {
 		url.append(URL_HEAD).append(nodeIp).append(":").append(port)
 				.append("/pools/default/rebalanceProgress");
 
-		String result = HttpClient.getCbaseManager(url.toString(), username,
+		String result = HttpClient.get(url.toString(), username,
 				password);
 		return new ApiResultObject(result,url.toString());
 	}
@@ -132,8 +235,8 @@ public class CbasePythonServiceImpl implements ICbasePythonService {
 		map.put("saslPassword", saslPassword);
 		map.put("replicaNumber", "1");
 
-		String result = HttpClient.postCbaseManager(url.toString(), nodeIp,
-				port, map, username, password);
+		String result = postCbaseManager(url.toString(), nodeIp,
+                port, map, username, password);
 		return new ApiResultObject(result,url.toString());
 	}
 
@@ -152,8 +255,8 @@ public class CbasePythonServiceImpl implements ICbasePythonService {
 		map.put("saslPassword", saslPassword);
 		map.put("bucketType", "memcached");
 
-		String result = HttpClient.postCbaseManager(url.toString(), nodeIp,
-				port, map, username, password);
+		String result = postCbaseManager(url.toString(), nodeIp,
+                port, map, username, password);
 		return new ApiResultObject(result,url.toString());
 	}
 
